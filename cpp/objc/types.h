9aaa5e0dce5233ed2802ecaac9d80f76b5228198
hlstwizard <h.l.s.t@163.com>
2021-11-11 17:47:25 +0800

    UI (#8)
    * undo/redo

--- cpp/objc/types.h
+++ cpp/objc/types.h
@@ -12,23 +12,4 @@ typedef short Loc;
 typedef int8_t Player;
 typedef int8_t Color;
 
-typedef struct {
-    Player owner;        //Owner of chain
-    short num_locs;      //Number of stones in chain
-    short num_liberties; //Number of liberties in chain
-} ChainData;
-
-typedef struct {
-    Player pla;
-    Loc loc;
-    Loc ko_loc;
-    uint8_t capDirs; //First 4 bits indicate directions of capture, fifth bit indicates suicide
-} MoveRecord;
-
-typedef struct {
-    uint64_t hash0;
-    uint64_t hash1;
-} KHash128; // as Hash128 is in the global namespace
-
-
 #endif /* types_h */
